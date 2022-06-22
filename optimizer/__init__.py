edc88bef91b5fa51bee16d23d8aae6305007105d
zmohtum <rajmohantumarada@gmail.com>
2022-06-22 15:00:30 +0200

    Optimizer changes

--- optimizer/__init__.py
+++ optimizer/__init__.py
@@ -1,6 +1,9 @@
 from torch import optim
-
+from .adam_Hd import Adam_HD
+from .sgd_Hd import SGD_HD
 
 Optimizers = {
-    'adam': optim.Adam
+    'adam': optim.Adam,
+    'adam_Hd' : Adam_HD
+    #'sgd_Hd' : SGD_HD
 }
