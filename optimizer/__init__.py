2622706df94da37db9ffbc6300b7612e69fc324e
zmohtum <rajmohantumarada@gmail.com>
2022-06-23 22:44:17 +0200

    adafactor Optimiser

--- optimizer/__init__.py
+++ optimizer/__init__.py
@@ -3,11 +3,13 @@ from .adam_Hd import Adam_HD
 from .sgd_Hd import SGD_HD
 from .op_adam_lop_adam import op_Adam_lop_Adam
 from .adamw import AdamW
+from .adafactor import Adafactor
 
 Optimizers = {
-    'adam': optim.Adam,
+    #'adam': optim.Adam,
     #'adam_op_adam_hd' : op_Adam_lop_Adam
     #'adam_Hd' : Adam_HD
     #'sgd_Hd' : SGD_HD
-    'Adam_w' : AdamW
+    #'Adam_w' : AdamW
+    'Adam_R' : Adafactor
 }
