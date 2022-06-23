3d1848b81c20063323eb77b9701b712f54dbc5ec
Christen M <mrkristen@gmail.com>
2022-06-23 19:40:02 +0200

    bug fixes

--- optimizer/__init__.py
+++ optimizer/__init__.py
@@ -4,7 +4,7 @@ from .sgd_Hd import SGD_HD
 from .op_adam_lop_adam import op_Adam_lop_Adam
 
 Optimizers = {
-    #'adam': optim.Adam,
+    'adam': optim.Adam,
     #'adam_op_adam_hd' : op_Adam_lop_Adam
     #'adam_Hd' : Adam_HD
     #'sgd_Hd' : SGD_HD
